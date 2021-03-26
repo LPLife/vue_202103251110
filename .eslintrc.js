0ef8154d628b08c82cb29ab9ccf4a1e0048e25a9
lupei <peri.lu@mart.blue>
2021-03-26 18:23:22 +0800

    ...

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    root: true,
+    env: {
+        node: true
+    },
+    plugins: ['prettier'],
+    extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+    parserOptions: {
+        parser: "babel-eslint",
+        ecmaVersion: 7,
+        sourceType: "module",
+        ecmaFeatures: {
+          // 添加ES特性支持，使之能够识别ES6语法
+          jsx: true,
+        },
+      },
+    rules: {
+        'prettier/prettier': 'error',
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
 };
---
+++ .stylelintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    extends: "stylelint-config-standard",
+    rules: {
+        "indentation": 4,
+        "color-hex-length": "long",
+        "color-hex-case": "lower",
+        "length-zero-no-unit": true,
+        "selector-pseudo-element-colon-notation": "single",
+        "no-descending-specificity": null,
+        "declaration-colon-newline-after": null,
+        "color-no-invalid-hex": true,
+        "color-hex-length": "long",
+        "color-hex-case": "lower",
+        "unit-whitelist": ["em", "rem", "%", "s", "px"],
+        "declaration-colon-newline-after": null,
+    },
+  };
+  
+  
